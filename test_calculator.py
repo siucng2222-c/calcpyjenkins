9323e414336d4e8f27e3b2de170869ad6db98000
S C NG (CLOUD-ISD-OOCLL/HKG) <NGSC@oocl.com>
2023-05-05 12:28:03 +0800

    test fail unittest

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ class TestCalculatorMethods(unittest.TestCase):
     def test_upper(self):
         calculator = Calculator()
         # self.assertEqual(3*4, 12)
-        self.assertEqual(calculator.multipy(3,4), 12)
+        self.assertEqual(calculator.multipy(3,4), 1)
 
 
 if __name__ == '__main__':
